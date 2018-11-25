2179eb581a04e43ff624ae22ea11a6427310b798
Manish <manishyadav12354@gmail.com>
2018-11-25 13:19:25 +0530

    Using heroku provided port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const app = require('./app.js');
 const server = http.createServer(app);
 server.listen(PORT, () => console.log(`server is listening ${PORT}`));
